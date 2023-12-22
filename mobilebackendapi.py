98bc433cf29ea09b8ae6671e439d121a6b5a7480
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2023-12-22 16:13:49 +0100

    Update mobilebackendapi.py

--- mobilebackendapi.py
+++ mobilebackendapi.py
@@ -1,14 +1,15 @@
-from contextlib import asynccontextmanager
-from typing import List, Optional, Dict
-
-import sqlalchemy.types
-import uvicorn
-from fastapi import FastAPI, HTTPException, Depends, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from pydantic import json
-from sqlalchemy import Column
-from sqlmodel import create_engine, Session, SQLModel, Field, select
 import logging
+from contextlib import asynccontextmanager
+from typing import List, Optional
+
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.params import Depends
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from pydantic import json, BaseModel
+from sqlmodel import create_engine, Session, SQLModel, Field
+from starlette import status
+from starlette.responses import FileResponse
 
 sqlite_file_name = "mobile_backgrounds.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -20,7 +21,7 @@ def create_db_and_tables():
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def app_lifespan(app: FastAPI):
     print("Startup event")
     try:
         create_db_and_tables()
@@ -29,82 +30,146 @@ async def lifespan(app: FastAPI):
         print("Shutdown event")
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=app_lifespan, description="Image Management API", version="0.1.0")
 logger = logging.getLogger(__name__)
 log_config = uvicorn.config.LOGGING_CONFIG
 log_config["formatters"]["access"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
 log_config["formatters"]["default"]["fmt"] = "%(asctime)s - %(levelname)s - %(message)s"
+
 uvicorn.run(app, log_config=log_config)
 
 security = HTTPBasic()
 
 
-class Device(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    device_id: str
-    api_key: str
-
-
-class ListType(sqlalchemy.types.TypeDecorator):
-    impl = sqlalchemy.types.String
-
-    def process_bind_param(self, value, dialect):
-        return json.dumps(value)
-
-    def process_result_value(self, value, dialect):
-        return json.loads(value) if value is not None else []
-
-
-class Image(SQLModel, table=True):
-    image_id: Optional[int] = Field(default=None, primary_key=True)
-    image: bytes
-    image_metadata: List[Dict[str, str]] = Field(sa_column=Column(ListType))
-
-
-def get_db():
-    with Session(engine) as session:
-        yield session
-
-
-def get_device_by_id(db: Session, device_id: str):
-    device = db.exec(select(Device).where(Device.device_id == device_id)).first()
-    if not device:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Device not found")
-    return device
-
-
-def authenticate_user(db: Session, credentials: HTTPBasicCredentials):
-    device = get_device_by_id(db, credentials.username)
-    if device.api_key != credentials.password:
+def authenticate_user(session: Session, credentials: HTTPBasicCredentials):
+    user = read_user_by_name(credentials.username, session=session)
+    if credentials.password != user.password:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
 
-@app.post("/devices", response_model=Device)
-def register_device(device: Device, db: Session = Depends(get_db)):
-    db.add(device)
-    db.commit()
-    db.refresh(device)
-    return device
+class User(BaseModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    password: str
+    role: str
 
 
-@app.get("/devices", response_model=List[Device])
-def list_devices(db: Session = Depends(get_db), credentials: HTTPBasicCredentials = Depends(security)):
-    authenticate_user(db, credentials)
-    devices = db.exec(select(Device)).all()
-    return devices
+class Device(BaseModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    user_id: int
 
 
-@app.get("/images", response_model=List[Image])
-def list_images(db: Session = Depends(get_db), credentials: HTTPBasicCredentials = Depends(security)):
-    authenticate_user(db, credentials)
-    images = db.exec(select(Image)).all()
-    return images
+class ApiKey(BaseModel):
+    api_key: str
 
 
-@app.get("/images/{id}", response_model=Image)
-def get_image(image_id: int, db: Session = Depends(get_db), credentials: HTTPBasicCredentials = Depends(security)):
-    authenticate_user(db, credentials)
-    image = db.exec(select(Image).where(Image.image_id == image_id)).first()
+class ImageMetadata(BaseModel):
+    metadata: dict
+
+
+class Image(BaseModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    metadata_id: int
+    file_path: str
+
+
+class ImageWithMetadata(BaseModel):
+    id: int
+    metadata: dict
+
+
+@app.post("/users", response_model=User)
+def create_user(user: User, session: Session = Session(engine), credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate_user(session, credentials)
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@app.get("/users", response_model=List[User])
+def read_users(session: Session = Session(engine), credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate_user(session, credentials)
+    users = session.query(User).all()
+    return users
+
+
+@app.get("/users/{user_id}", response_model=User)
+def read_user(user_id: int, session: Session = Session(engine), credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate_user(session, credentials)
+    user = (session.query(User).filter(User.id == user_id).first())
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@app.get("/users/name/{username}", response_model=User)
+def read_user_by_name(username: str, session: Session = Session(engine),
+                      credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate_user(session, credentials)
+    user = session.query(User).filter(User.username == username).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@app.put("/users/{user_id}", response_model=User)
+def update_user(user_id: int, user: User, session: Session = Session(engine),
+                credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate_user(session, credentials)
+    db_user = session.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_user.username = user.username
+    db_user.password = user.password
+    db_user.role = user.role
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, session: Session = Session(engine),
+                credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate_user(session, credentials)
+    db_user = session.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(db_user)
+    session.commit()
+
+
+@app.post("/devices", response_model=ApiKey)
+def register_device(device: Device, session: Session = Session(engine),
+                    credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate_user(session, credentials)
+    session.add(device)
+    session.commit()
+    session.refresh(device)
+    api_key = ApiKey(api_key=f"api_key_{device.id}")
+    return api_key
+
+
+@app.get("/images", response_model=List[ImageWithMetadata])
+def read_images(session: Session = Session(engine), credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate_user(session, credentials)
+    images = session.query(Image).all()
+    images_with_metadata = []
+    for image in images:
+        metadata = session.query(ImageMetadata).filter(ImageMetadata.id == image.metadata_id).first()
+        metadata_dict = json.loads(metadata.metadata)
+        images_with_metadata.append(ImageWithMetadata(id=image.id, metadata=metadata_dict, file_path=image.file_path))
+    return images_with_metadata
+
+
+@app.get("/images/{image_id}")
+def read_image(image_id: int, session: Session = Session(engine),
+               credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate_user(session, credentials)
+    image = session.query(Image).filter(Image.id == image_id).first()
     if not image:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Image not found")
-    return image
+        raise HTTPException(status_code=404, detail="Image not found")
+    # metadata = session.query(ImageMetadata).filter(ImageMetadata.id == image.metadata_id).first()
+    # metadata_dict = json.loads(metadata.metadata)
+    return FileResponse(image.file_path, media_type="image/png")
